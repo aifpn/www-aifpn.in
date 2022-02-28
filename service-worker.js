c33245250cfd8c9ab5826b89767913a562589368
Utkrisht Singh Chauhan <koq.utx10101@intacht.com>
2022-02-28 10:58:51 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const timestamp = 1645986518261;
+const timestamp = 1646026125534;
 const build = [
-  "/_app/start-0add7617.js",
+  "/_app/start-73d2db77.js",
   "/_app/pages/__layout.svelte-623b95ac.js",
   "/_app/error.svelte-060fe7c8.js",
   "/_app/pages/index.svelte-7ac8d98d.js",
@@ -16,7 +16,7 @@ const build = [
   "/_app/pages/_locale_/activities/meat-processing.svelte-fa41d624.js",
   "/_app/pages/_locale_/gallery.svelte-f0c5f44f.js",
   "/_app/pages/_locale_/about/index.svelte-91d44c5d.js",
-  "/_app/pages/_locale_/about/officers-directory.svelte-ca5d0a4d.js",
+  "/_app/pages/_locale_/about/officers-directory.svelte-8e1321fe.js",
   "/_app/pages/_locale_/about/key-persons.svelte-457a54aa.js",
   "/_app/pages/_locale_/about/importance.svelte-c13c79b8.js",
   "/_app/pages/_locale_/about/objectives.svelte-e8ff9930.js",
